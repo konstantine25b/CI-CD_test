89ede4267e1c7253fccdb2f0cb56dcd2d955c3f9
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-23 17:30:07 +0400

    added tests, corrected views.py and added urls and gitignore

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookModelTest(TestCase):
+    def test_book_category(self):
+        book = Book.objects.create(title = 'book 1' , author = 'kosa')
+        
+        self.assertEqual(str(book) ,"book 1")
